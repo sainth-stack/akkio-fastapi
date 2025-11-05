ecf3bfdd9c2941bea8b512bfeaac3e18f5ff9f2b
sainath-stylishop <sainath@stylishop.com>
2025-11-05 16:30:22 +0530

    updated changes

--- final_akio_apis.py
+++ final_akio_apis.py
@@ -4863,5 +4863,5 @@ async def read_data_from_db(
 
 if __name__ == "__main__":
     import uvicorn
-
-    uvicorn.run("final_akio_apis:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("final_akio_apis:app", host="0.0.0.0", port=port)
